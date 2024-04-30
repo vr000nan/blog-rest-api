9305557e99115ff941a427671044c5c039f4104b
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-04-30 13:42:37 -0400

    login user added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -46,6 +46,40 @@ function signUp(req, res) {
     })
 }
 
+
+function login(req, res) {
+    models.User.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user === null) {
+            res.status(401).json({
+                message: "Invalid credentials!"
+            })
+        } else {
+            bcryptjs.compare(req.body.password, user.password, function (err, result) {
+                if (result) {
+                    const token = jwt.sign({
+                        email: user.email,
+                        userId: user.id
+                    }, 'secret', function (err, token) {
+                        res.status(200).json({
+                            message: "Logged in successfully!",
+                            token: token
+                        })
+                    });
+                } else {
+                    res.status(401).json({
+                        message: "Invalid credentials!"
+                    })
+                }
+            });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            message: "Something went wrong while logging in..."
+        })
+    });
+}
+
 module.exports = {
-    signUp: signUp
+    signUp: signUp,
+    login: login
 }
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ const userController = require('../controllers/user.controller');
 const router = express.Router();
 
 router.post('/sign-up', userController.signUp);
+router.post('/login', userController.login);
 
 module.exports = router;
