95ec46e75a8e63902de99d195d4135ea6d4fd294
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-04-30 13:03:57 -0400

    added error handling for duplicate password

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,36 +3,47 @@ const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 function signUp(req, res) {
-    bcryptjs.genSalt(10, function (err, salt) {
-        if (err) {
-            return res.status(500).json({
-                message: "Error generating salt: " + err.message
+
+    models.User.findOne({ where: { email: req.body.email } }).then(result => {
+        if (result) {
+            res.status(409).json({
+                message: "Email already exists!"
+            });
+        } else {
+            bcryptjs.genSalt(10, function (err, salt) {
+                if (err) {
+                    return res.status(500).json({
+                        message: "Error generating salt: " + err.message
+                    });
+                }
+                bcryptjs.hash(req.body.password, salt, function (err, hash) {
+                    if (err) {
+                        return res.status(500).json({
+                            message: "Error hashing password: " + err.message
+                        });
+                    }
+                    const user = {
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: hash
+                    };
+                
+                    models.User.create(user).then(result => {
+                        res.status(201).json({
+                            message: "User created successfully",
+                        });
+                    }).catch(error => {
+                        res.status(500).json({
+                            message: "Something went wrong while creating user...",
+                            error: error.message
+                        });
+                    });
+                });
             });
         }
-        bcryptjs.hash(req.body.password, salt, function (err, hash) {
-            if (err) {
-                return res.status(500).json({
-                    message: "Error hashing password: " + err.message
-                });
-            }
-            const user = {
-                name: req.body.name,
-                email: req.body.email,
-                password: hash
-            };
-        
-            models.User.create(user).then(result => {
-                res.status(201).json({
-                    message: "User created successfully",
-                });
-            }).catch(error => {
-                res.status(500).json({
-                    message: "Something went wrong while creating user...",
-                    error: error.message
-                });
-            });
-        });
-    });
+    }).catch(error => {
+        console.error(error);
+    })
 }
 
 module.exports = {
