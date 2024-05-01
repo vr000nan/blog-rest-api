eab85009e699f53d86fedb89d48667dec7bb8a66
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-05-01 18:41:05 -0400

    image upload feature added

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app = express();
 
 const postsRoute = require('./routes/posts');
 const userRoute = require('./routes/user');
+const imageRoute = require('./routes/images');
 
 app.use(bodyParser.json());
 app.use("/posts", postsRoute);
 app.use("/user", userRoute);
+app.use("/images", imageRoute);
 
 module.exports = app;
